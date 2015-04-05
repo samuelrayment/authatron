c40a2f0f2bfd18e39b09b53d12ffb6e269f7966a
Samuel Rayment <samrayment@gmail.com>
2015-04-05 19:09:42 +0100

    Added some further docs

--- doc.go
+++ doc.go
@@ -24,5 +24,36 @@
 Package Authatron provides authentication interfaces for authenticating users
 in go.  Currently fake (fixed password) and LDAP authentication is supported.
 
+Configuration
+
+Authatron supports configuration by creating an AuthConfig programmatically,
+a default empty config can be created using NewLDAPAuthenticatorFromConfig.
+AuthConfig is marked up using struct tags to be loaded from a toml file using:
+https://github.com/BurntSushi/toml
+
+Configuration can also be loaded from environment variables using
+UpdateConfigFromEnvironmentVariables which can take a prefix to configure a
+prefix for the environment variables.
+
+Interface
+
+Once configured NewAuthenticateServiceFromConfig can be used to create a new
+AuthenticateService.  The AuthenticateService consists of two smaller interfaces
+the UserStore and Authenticator.  The UserStore is responsible for storing and
+retrieving user credentials and the Authenticator is responsible for initially
+authenticating a user.
+
+UserStore - Currently the only implementation of UserStore is a cookie user store
+using http://github.com/gorilla/securecookie.
+
+Authenticator - There are currently two Authenticator implementations
+
+	- 'dummy' which allows a password to be set and will authenticate all users using that password.
+	- 'ldap' which authenticates users against and LDAP server.
+
+Integrating
+
+Soon...
+
 */
 package authatron
